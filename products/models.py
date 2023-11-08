49b13a9d41c457bca7ee40af80a0612e55d0d72a
A-Gr33n <>
2023-11-08 13:58:36 +0000

    inital commit

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self)
+        return self.name
+    
+    def get_frienldy_name(self):
+        return self.friendly_name 
+
+class ProductAttribute(models.Model):
+    
+     
+    def __str__(self):
+        return self.name
+    
