c09abd8f867316170e36ee3c84a55c9edca139c3
A-Gr33n <>
2023-11-09 19:18:41 +0000

    adjusted custom models

--- products/models.py
+++ products/models.py
@@ -19,41 +19,41 @@ class Product(models.Model):
     product_description = models.DecimalField(max_digits=6, decimal_places=2)
     image=models.ImageField(upload_to='product_image/')
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    size=models.ForeignKey(Size,on_delete=models.CASCADE)
+    size=models.ForeignKey('Size',on_delete=models.CASCADE)
     stock = models.PositiveIntegerField()
 
     def __str__(self):
         return self.name
 
 class Order(models.Model):
-    user = models.ForeginKey(User, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product, through='OrderItem')
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    product = models.ManyToManyField(Product, through='OrderItem')
     ordered_date = models.DateTimeField(auto_now=True)
-    total_price = models.DecimalField(max_digits=10, decimal_place=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
     order_status = models.CharField(max_length=20, default='Pending')
+    quantity = models.PositiveIntegerField()
 
     def __str__(self):
         return f'Order #{self.pk} by {self.user.username}'
 
-class OrderItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
-    item_price = models.DecimalField(max_digits=10, decimal_places=2)
     
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    shipping_address = models.TextField()
-    phone_number = models.CharField(max_length=15)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    fitness_goals = models.CharField(max_length=255)
+    preferred_workout_type = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
 
     def __str__(self):
         return self.user.username
 
 class Review(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    rating = models.FloatField()
+    review_text = models.TextField()
+    date = models.DateField()
 
     def __str__(self):
         return f'Review by {self.user.username} for {self.product.name}'
