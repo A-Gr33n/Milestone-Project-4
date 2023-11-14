a63e84366da705636e8dc8412d0f4a6d89728d3a
A-Gr33n <>
2023-11-14 22:02:58 +0000

    inital commit

--- products/models.py
+++ products/models.py
@@ -13,13 +13,13 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    product_name = models.CharField(max_length=254)
-    product_description = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(null=True, blank=True)
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     stock = models.PositiveIntegerField()
 
     def __str__(self):
