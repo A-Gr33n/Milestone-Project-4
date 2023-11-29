f97a503aca7055d05df5dbae30b86e83f042df67
A-Gr33n <>
2023-11-29 14:57:42 +0000

    installed crispy forms

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from . forms import OrderForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, "There's nothing in your bag at the moment")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51O6tADDxkJiNr58k4TCIGwe2zf8s3uN34HI67CQjSSLbIsJ5zcQpsDOyLFBmOIeD1vqGHkNYTbasO7WfHdHyzSrl00kjdX0Zqg',
+        'client_secret': 'CLIENT_SECRET',
+    }
+
+    return render(request, template, context)
+
+
