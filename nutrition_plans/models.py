ab96c3186dcd1265c5136bacab2e677a81799e80
A-Gr33n <>
2023-12-01 14:36:46 +0000

    inital commit

--- nutrition_plans/models.py
+++ nutrition_plans/models.py
@@ -1,8 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
 
 
-class nutrition_plans(models.Model):
-  name = models.CharField(max_length=254)
-  description = models.TextField()
+
+class Meal(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class NutritionPlan(models.Model):
+    goal = models.CharField(max_length=50)
+    meals = models.ManyToManyField(Meal)
+
+    def __str__(self):
+        return self.goal
+        
+    def __str__(self):  
+        return self.name
