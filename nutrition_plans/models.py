5124b781370d8c305577d0c509562c46f5e24997
A-Gr33n <>
2023-12-02 00:53:37 +0000

    50% completed nutrition plans fields

--- nutrition_plans/models.py
+++ nutrition_plans/models.py
@@ -20,6 +20,10 @@ class Meal(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+
 
     def __str__(self):
         return self.name
