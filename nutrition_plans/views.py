2f6c3173a4ebab2923c84e2af355e48d1bdfea0f
A-Gr33n <>
2023-12-05 00:48:08 +0000

    initial commit

--- nutrition_plans/views.py
+++ nutrition_plans/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Meal
 
 # Create your views here.
+
+
+def all_meals(request):
+    """ A view to show all meals, including sorting and search queries """
+
+    meals = Meal.objects.all()
+
+    context = {
+        'meals': meals,
+    }
+
+    return render(request, 'meals/nutrition_plans.html', context)
