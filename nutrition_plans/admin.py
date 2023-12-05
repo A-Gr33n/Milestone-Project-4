dca886f1f662de51ef94b8d33590d0ee5e5d4271
A-Gr33n <>
2023-12-05 15:00:41 +0000

    inital commit

--- nutrition_plans/admin.py
+++ nutrition_plans/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Meal, Category
-# Register your models here.
+from.models import Meal, Category
 
 admin.site.register(Meal)
 admin.site.register(Category)
