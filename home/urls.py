07998ec3ff056ba64e9c60bce3801f99d274ffd6
A-Gr33n <>
2024-01-11 20:16:52 +0000

    intial commit

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home') , 
-    path('fitnessgalore/', include('fitnessgalore.urls')),
+    path('', views.index, name='home') 
 ]
